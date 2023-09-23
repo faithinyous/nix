34ddf49bf76e204472e817fdc0cd6f33663c58a7
Apiporn Simapornchai <faithinyouuu@gmail.com>
2023-09-23 18:36:05 +0700

    remove fd from decryption and use grep

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -6,8 +6,12 @@
 #	echo "Decrypted $i"
 #done
 
-find . -name "*.sop*" -print | while read -r line; do
+find . -name "*.sop*" -print | while read -r i; do
+	if [[ $i == "./.sops.yaml" ]]; then
+		continue
+	fi
 	decrypted_file=${i//.sops/}
 	sops -d "$i" >"$decrypted_file"
-	echo "Decrypted $i"
+	echo "Decrypted $decrypted_file"
+	#	echo "Encrypted $i"
 done
