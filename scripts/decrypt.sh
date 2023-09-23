77f68481dafce2f23a9db14d55bbdd9ae1f85736
Apiporn Simapornchai <faithinyouuu@gmail.com>
2023-09-23 18:29:28 +0700

    update decryption

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-for i in $(fd sops 'modules/tools/' -t f -E 'sops.nix'); do
+#for i in $(fd sops 'modules/tools/' -t f -E 'sops.nix'); do
+#	decrypted_file=${i//.sops/}
+#	sops -d "$i" >"$decrypted_file"
+#	echo "Decrypted $i"
+#done
+
+find . -name "*.sop*" -print | while read -r line; do
 	decrypted_file=${i//.sops/}
 	sops -d "$i" >"$decrypted_file"
 	echo "Decrypted $i"
