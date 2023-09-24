5f58b469f9f3a64d098fe6d7245f89e4aa8f9c2c
Apiporn Simapornchai <faithinyouuu@gmail.com>
2023-09-24 19:05:13 +0700

    remove what i dont use in mac

--- scripts/mac/02_install_mac.sh
+++ scripts/mac/02_install_mac.sh
@@ -12,39 +12,39 @@ brew install mas
 brew install mpv
 
 # cloud storage
-brew install --cask dropbox
-brew install --cask syncthing
+#brew install --cask dropbox
+#brew install --cask syncthing
 
 # communication
-brew install --cask adium
+#brew install --cask adium
 brew install --cask discord
 brew install --cask microsoft-teams
-brew install --cask slack
-brew install --cask whatsapp
+#brew install --cask slack
+#brew install --cask whatsapp
 brew install --cask zoom
 
 # data
 # brew install --cask qgis
-brew install --cask tad
-brew install apache-spark
+#brew install --cask tad
+#brew install apache-spark
 
 # dev tools
-brew install --cask datagrip
+#brew install --cask datagrip
 brew install --cask docker
 # brew install --cask fig
-brew install --cask fork
-brew install --cask gitup
-brew install --cask httpie
+#brew install --cask fork
+#brew install --cask gitup
+#brew install --cask httpie
 brew install --cask intellij-idea
 brew install --cask kitty
-brew install kui
-brew install --cask microsoft-remote-desktop
+#brew install kui
+#brew install --cask microsoft-remote-desktop
 brew install --cask mongodb-compass
 brew install --cask openlens
 brew install --cask postman
 # brew install --cask rio
 brew install --cask tailscale
-brew install --cask visual-studio-code
+#brew install --cask visual-studio-code
 
 # entertainment
 brew install --cask calibre
@@ -63,17 +63,17 @@ brew install --cask qlmarkdown
 brew install --cask cron
 brew install --cask macdown
 brew install --cask obsidian
-brew install --cask sleek
+#brew install --cask sleek
 brew install --cask todoist
 brew install --cask todotxt
 
 # utilities
-brew install --cask alfred
+#brew install --cask alfred
 brew install --cask angry-ip-scanner
 brew install --cask anydesk
 brew install --cask appcleaner
 brew install --cask balenaetcher
-brew install --cask bitwarden
+#brew install --cask bitwarden
 brew install --cask cheatsheet
 brew install --cask cloudflare-warp
 brew install --cask coconutbattery
@@ -81,27 +81,27 @@ brew install --cask flameshot
 brew install --cask hiddenbar
 brew install --cask hot
 brew install --cask kap
-brew install --cask keepassxc
+#brew install --cask keepassxc
 brew install --cask keepingyouawake
 brew install --cask maccy
-brew install --cask michaelvillar-timer
-brew install --cask protonvpn
-brew install --cask raspberry-pi-imager
-brew install --cask shifty
+#brew install --cask michaelvillar-timer
+#brew install --cask protonvpn
+#brew install --cask raspberry-pi-imager
+#brew install --cask shifty
 brew install --cask spectacle
 brew install --cask stats
 brew install --cask the-unarchiver
 brew install --cask tyke
-brew install --cask yubico-yubikey-manager
+#brew install --cask yubico-yubikey-manager
 
 # peripheral software
-brew install --cask linearmouse
-brew install --cask logitech-camera-settings
+#brew install --cask linearmouse
+#brew install --cask logitech-camera-settings
 brew install --cask logitech-g-hub
 
 echo "================================= Install App Store applications ================================="
-mas install 1176895641 # spark
-mas install 1423210932 # flow - focus & pomodoro timer
-mas install 1451685025 # wireguard
+#mas install 1176895641 # spark
+#mas install 1423210932 # flow - focus & pomodoro timer
+#mas install 1451685025 # wireguard
 mas install 539883307  # LINE
-mas install 568494494  # pocket
+#mas install 568494494  # pocket
